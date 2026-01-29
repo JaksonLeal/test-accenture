b5893a11f66e93695e9d60ddfed7dfdc82d1a444
jakson leal <jleal338@unab.edu.co>
2026-01-29 14:56:06 -0500

    initial

---
+++ back-accenture/src/main/java/com/test/back_accenture/BackAccentureApplication.java
@@ -0,0 +1,13 @@
+package com.test.back_accenture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackAccentureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackAccentureApplication.class, args);
+	}
+
+}
---
+++ back-accenture/src/test/java/com/test/back_accenture/BackAccentureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.back_accenture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackAccentureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
